7e268046bc0ab6ac824a64c5ae651c497b86bf5d
BryonLH <bryonlh@gmail.com>
2019-07-01 23:33:49 -0700

    testing code from youtube

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -43,15 +43,36 @@ function init() {
 
 
 
+/**
+   * Sample JavaScript code for youtube.search.list
+   * See instructions for running APIs Explorer code samples locally:
+   * https://developers.google.com/explorer-help/guides/code_samples#javascript
+   */
 
-// $(".gif-button").on("click", function () {
-//   var character = $(this).attr("data-character");
-//   var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + giphyAPIKey + "&q=" + character + "+disney&limit=10&lang=en";
-//   axios({
-//       url: queryURL,
-//       method: "GET"
-//   }).then(function (response) {
-//       console.log(response);
-//       console.log(response.data.data);
-//       var results = response.data.data;
-//       var animationState = "still";
+  // function authenticate() {
+  //   return gapi.auth2.getAuthInstance()
+  //       .signIn({scope: "https://www.googleapis.com/auth/youtube.force-ssl"})
+  //       .then(function() { console.log("Sign-in successful"); },
+  //             function(err) { console.error("Error signing in", err); });
+  // }
+  // function loadClient() {
+  //   gapi.client.setApiKey("AIzaSyA7qkLvUXJLWyyZl97iOw5z7VNDjMzlh40");
+  //   return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
+  //       .then(function() { console.log("GAPI client loaded for API"); },
+  //             function(err) { console.error("Error loading GAPI client for API", err); });
+  // }
+  // // Make sure the client is loaded and sign-in is complete before calling this method.
+  // function execute() {
+  //   return gapi.client.youtube.search.list({
+  //     "part": "snippet",
+  //     "q": "lady gaga"
+  //   })
+  //       .then(function(response) {
+  //               // Handle the results here (response.result has the parsed body).
+  //               console.log("Response", response);
+  //             },
+  //             function(err) { console.error("Execute error", err); });
+  // }
+  // gapi.load("client:auth2", function() {
+  //   gapi.auth2.init({client_id: "YOUR_CLIENT_ID"});
+  // });
